cda50db421f95647a1575a7b7be2044842b771fc
Maciej Burek <mburek@primeo.pl>
2024-09-02 13:16:52 +0200

    e-commerce

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>e-commerce</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Features</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Pricing</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Disabled</a>
+            </li>
+            @auth
+            <li class="nav-item">
+                <a class="nav-link" href="#">Dashboard</a>
+            </li>
+
+            @else
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('login') }}">Logowanie</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Rejestracja</a>
+              </li>
+            @endauth
+          </ul>
+        </div>
+        @auth
+        <span class="navbar-text" style="margin-right: 5px">
+            Zalogowany jako: {{ auth()->user()->name }}
+        </span>
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+            <button type="submit">Wyloguj się</button>
+        </form>
+        @endauth
+      </nav>
+      @yield('content')
+</body>
